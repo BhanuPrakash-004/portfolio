263e2e70e1f7200219bf1cc9a6b5483b9b5e2a5a
b23cs004-004 <b23cs004@kitsw.ac.in>
2025-05-28 16:45:05 +0530

    changed info

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,10 +7,9 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm SBP</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          Hi, I'm Bhanu Prakash, a passionate tech enthusiast with a keen interest in exploring and understanding emerging technologies. I love diving deep into how things work and constantly seek opportunities to expand my knowledge in the world of software development. 
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
